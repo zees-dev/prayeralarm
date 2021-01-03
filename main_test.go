5cae4c4c8a2a5996fa3b74d25b4a4300846fbd82
zees-dev <dev.786zshan@gmail.com>
2021-01-04 03:11:23 +1300

    implemented minimal API integration test

--- main_test.go
+++ main_test.go
@@ -15,7 +15,7 @@ func TestGetTime(t *testing.T) {
 		l, _ := time.LoadLocation(tz)
 		want := time.Date(2021, 1, 1, 4, 14, 0, 0, l)
 
-		if want.String() != got.String() {
+		if got.String() != want.String() {
 			t.Errorf("want %s, got %s", want, got)
 		}
 	})
@@ -29,7 +29,7 @@ func TestGetTime(t *testing.T) {
 		l, _ := time.LoadLocation(tz)
 		want := time.Date(2021, 1, 1, 4, 14, 0, 0, l)
 
-		if want.String() != got.String() {
+		if got.String() != want.String() {
 			t.Errorf("want %s, got %s", want, got)
 		}
 	})
@@ -43,8 +43,32 @@ func TestGetTime(t *testing.T) {
 		l, _ := time.LoadLocation(lc)
 		want := time.Date(2021, 1, 1, 4, 14, 0, 0, l)
 
-		if want.String() != got.String() {
+		if got.String() != want.String() {
 			t.Errorf("want %s, got %s", want, got)
+		}
+	})
+}
+
+// TestGetMonthCalendar is an API integration test
+func TestGetMonthCalendar(t *testing.T) {
+	t.Run("gets calendar for Auckland NewZealand - successful API status with timezone", func(t *testing.T) {
+
+		got := getMonthCalendar("Auckland", "NewZealand", "0,0,0,0,0", 1, 1)
+
+		want := MonthlyAdhanCalenderResponse{Code: 200, Status: "OK"}
+
+		if got.Code != want.Code {
+			t.Errorf("want %d, got %d", want.Code, got.Code)
+		}
+
+		if got.Status != want.Status {
+			t.Errorf("want %s, got %s", want.Status, got.Status)
+		}
+
+		wantTz := "Pacific/Auckland"
+
+		if got.Data[0].Meta.Timezone != wantTz {
+			t.Errorf("want %s, got %s", wantTz, got.Data[0].Meta.Timezone)
 		}
 	})
 }
